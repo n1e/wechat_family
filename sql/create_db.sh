191d7cde8bea4f1a86b2c19d22bd107d75cc1b9d
n1e <aricover@foxmail.com>
2018-11-20 18:36:18 +0800

    modify sh

--- sql/create_db.sh
+++ sql/create_db.sh
@@ -4,6 +4,6 @@ pwd=$2
 cd create_table
 mysql -u${name} -p${pwd}<<EOF
 
-source /home/n1e/wechat_family/sql/create_table/create_db_user.sql
-source /home/n1e/wechat_family/sql/create_table/create_table.sql
+source /home/n1e/py/sql/create_table/create_db_user.sql
+#source /home/n1e/wechat_family/sql/create_table/create_table.sql
 EOF
