e4c6168e5ace6b3efcec1db78fadcd64eb8adf28
n1e <aricover@foxmail.com>
2018-11-20 18:19:43 +0800

    modify sql

--- sql/create_table/create_db_user.sql
+++ sql/create_table/create_db_user.sql
@@ -1,6 +1,17 @@
 create database if not exists LR_DB Character set UTF8;
-drop user 'lr'@'%';
+use LR_DB;
+drop procedure if exists pro_add_user;
+DELIMITER$$
+create procedure pro_add_user()
+BEGIN
+declare icnt int default 0;
+select count(*) into icnt from mysql.user where user = 'lr';
+if icnt = 1
+then drop user 'lr'@'%';
+end if;
 create user 'lr'@'%' identified by 'temp';
-
 grant all privileges on LR_DB.* to 'lr'@'%' with grant option;
 flush privileges;
+END$$
+
+call pro_add_user;
